3b7d54f8e1c0141bcda70dc8fdeb7bd7b6ae8738
lfof <113575617+lfof@users.noreply.github.com>
2022-10-24 21:36:54 -0700

    First JavaScript Commit

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, World!")
+document.write('Hello, World!');    //This gives the alert button when you reach the site for the first time
+
+var A = " This is a string";    //Defining variable A
+document.write(A);              
+
+var B = "Concatenated Text"     //Definning variable B
+document.write(A + "" +B);      //Concatenating variables A + B     
+
+var Make = "Subaru", SUV = "Ascent", Cars = "Legacy", Performance = "WRX";  //Defining multiple variables
+document.write(Performance);                                                //Calling variable Performance
+
+document.write(3+3);    //First expression adding 3+3 which will give the answer 6 in the index html
