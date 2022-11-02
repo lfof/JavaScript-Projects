134438647e8698a32bcfd08e07c58cc34b91cb8c
lfof <113575617+lfof@users.noreply.github.com>
2022-11-02 00:22:24 -0700

    scope_time_functions

---
+++ Basic-JavaScript-Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,33 @@
+var X = 2;
+function Multiply_numbers_1() {
+    document.write(20 * X + "<br>");
+}
+Multiply_numbers_1();
+
+function Multiply_numbers_2() {
+    var X = 3;
+    document.write(20 * X + "<br>");
+}
+Multiply_numbers_2();
+
+
+function Multiply_numbers_3() {
+    console.log(Y * 10);
+}
+Multiply_numbers_3();
+
+function get_Date() {
+    if (new Date() .getHours() < 16) {
+    document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+function get_Date() {
+    if (new Date() .getMinutes() < 30) {
+    document.getElementById("Time").innerHTML = "How long is this?";
+    }
+}
+
+function Age_Function() {
+    Age = document.getElementById
+}
