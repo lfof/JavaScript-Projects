d4591afc1321536bb7c006efdf6ca2fab3201c5f
lfof <113575617+lfof@users.noreply.github.com>
2022-11-02 22:26:32 -0700

    JS project7 commit

--- Basic-JavaScript-Projects/Project7_scope_time_function/JS/main.js
+++ Basic-JavaScript-Projects/Project7_scope_time_function/JS/main.js
@@ -1,33 +1,52 @@
-var X = 2;
-function Multiply_numbers_1() {
-    document.write(20 * X + "<br>");
+var X = 2;                      //defining a global variable
+function Multiply_numbers_1() { //applying multiplying function
+    document.write(20 * X + "<br>");    //multiplication operation
 }
-Multiply_numbers_1();
+Multiply_numbers_1(); //multiplication result
 
-function Multiply_numbers_2() {
-    var X = 3;
-    document.write(20 * X + "<br>");
+function Multiply_numbers_2() { //applying multiplying function
+    var X = 3;                  //defining local variable
+    document.write(20 * X + "<br>");    //multiplcation operation
 }
-Multiply_numbers_2();
+Multiply_numbers_2(); //multiplication result
 
 
-function Multiply_numbers_3() {
-    console.log(Y * 10);
-}
-Multiply_numbers_3();
+function Multiply_numbers_3() { //applying multiplication function
+    console.log(Y * 10);        //multiplication operation
+}                               //intetionally did not give the variable a value
+Multiply_numbers_3();           //to pull error and debug with console.log
+
+
 
 function get_Date() {
-    if (new Date() .getHours() < 16) {
+    if (new Date() .getHours() > 16) {
     document.getElementById("Greeting").innerHTML = "How are you today?";
     }
 }
 
-function get_Date() {
-    if (new Date() .getMinutes() < 30) {
-    document.getElementById("Time").innerHTML = "How long is this?";
-    }
-}
 
 function Age_Function() {
-    Age = document.getElementById
+    Age = document.getElementById("Age").value;
+    if (Age >= 16) {
+        Vote = "You are old enough to drive!";
+    }
+    else {
+        Vote = "You are not old enough to drive!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+function Time_function() {              //applying time funciton
+    var Time = new Date().getHours();   //defining varialbe and new object
+    var Reply;                          //defining variable reply if 
+    if (Time < 12 == Time > 0) {        //time is between 12 am and 12 pm
+        Reply = "It is mornig time!";
+    }
+    else if (Time >= 12 == Time < 18) { //defining variable reply if 
+        Reply = "It is afternoon.";     //time is between 12 pm and 16 pm
+    }
+    else {                              //defining variable reply when
+        Reply = "It is almost time for bed.";   //time is between 16 pm and 12 am
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
