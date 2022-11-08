c3faa1dd05a7a8e9445f9710ff2e850d12c34c1d
lfof <113575617+lfof@users.noreply.github.com>
2022-11-07 23:15:24 -0800

    Canvas

--- Basic-JavaScript-Projects/JavaScript_Projects/Javascript/JavaScript_1.js
+++ Basic-JavaScript-Projects/JavaScript_Projects/Javascript/JavaScript_1.js
@@ -26,3 +26,19 @@ function Color_Function() {
     }
     document.getElementById("Output").innerHTML = Color_Output;
 }
+
+function Change_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+
+var c = document.getElementById("my_Canvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
